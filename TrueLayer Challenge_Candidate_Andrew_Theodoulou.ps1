cfa2636e57ac61d689673f998bee7e30a3b8986e
lastspartan23 <54273932+lastspartan23@users.noreply.github.com>
2019-08-19 13:26:17 +0100

    Add files via upload

---
+++ TrueLayer Challenge_Candidate_Andrew_Theodoulou.ps1
@@ -0,0 +1,23 @@
+﻿New-Item -Path 'C:\Test\TrueLayer' -ItemType directory
+#date-time
+$(Get-Date) | out-file 'C:\Test\TrueLayer\TLTest.txt'
+
+#Script Name
+$MyInvocation.MyCommand | out-file 'C:\Test\TrueLayer\TLTest.txt' -Append
+
+#user
+"$env:userdomain $env:username" | out-file 'C:\Test\TrueLayer\TLTest.txt' -Append
+
+#pid
+$pid | out-file 'C:\Test\TrueLayer\TLTest.txt' -Append
+
+#RAM
+(Get-WmiObject Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum).Sum | Out-File 'C:\Test\TrueLayer\TLTest.txt'  -Append
+(Get-WmiObject -class "Win32_PhysicalMemoryArray").MaxCapacity | Out-File 'C:\Test\TrueLayer\TLTest.txt' -Append
+
+#IP
+[string] ((test-connection -Count 1 $env:computername).IPV4Address) | Out-File 'C:\Test\TrueLayer\TLTest.txt' -Append
+
+#List files and folders
+Get-ChildItem -Recurse 'C:\Test' | Out-File 'C:\Test\TrueLayer\TLTest.txt' -Append
+
